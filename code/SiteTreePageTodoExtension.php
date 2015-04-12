5934646dec0529373b4e809363d09890bee814dc
Werner Krauss <werner.krauss@netwerkstatt.at>
2015-04-12 20:13:28 +0200

    only add field if it does not exist we get a nasty error otherwise: [User Error] collateDataFields() I noticed that a field called 'ToDo' appears twice in your form

--- code/SiteTreePageTodoExtension.php
+++ code/SiteTreePageTodoExtension.php
@@ -7,10 +7,12 @@ class SiteTreePageTodoExtension extends DataExtension {
 	);
 	
 	function updateCMSFields(FieldList $fields) {
-		$fields->addFieldToTab('Root', new Tab(_t('SiteTree.TABTODO', 'To-do') . ($this->owner->ToDo ? '**' : ''),
-			new LiteralField("ToDoHelp", _t('SiteTree.TODOHELP', "<p>You can use this to keep track of work that needs to be done to the content of your site.  To see all your pages with to do information, open the 'Site Reports' window on the left and select 'To Do'</p>")),
-			new TextareaField("ToDo", "", 10)
-		));
+		if (!$fields->hasField('ToDo')) {
+			$fields->addFieldToTab('Root', new Tab(_t('SiteTree.TABTODO', 'To-do') . ($this->owner->ToDo ? '**' : ''),
+				new LiteralField("ToDoHelp", _t('SiteTree.TODOHELP', "<p>You can use this to keep track of work that needs to be done to the content of your site.  To see all your pages with to do information, open the 'Site Reports' window on the left and select 'To Do'</p>")),
+				new TextareaField("ToDo", "", 10)
+			));
+		}
 	}
 	
 	function updateFieldLabels(&$labels) {
